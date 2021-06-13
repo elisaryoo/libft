5066236ac23b69fac154f4958dbcfc034cfe0e46
Elisa <elisaymh@gmail.com>
2021-06-13 14:12:42 -0300

    yogurt

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: eryoo <eryoo@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 23:20:08 by eryoo             #+#    #+#             */
-/*   Updated: 2021/06/13 14:06:59 by eryoo            ###   ########.fr       */
+/*   Updated: 2021/06/13 14:12:25 by eryoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,8 @@ size_t				ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
-char				*ft_strnstr(const char *haystack, const char *needle,size_t len);
+char				*ft_strnstr(const char *haystack, const char *needle,
+					size_t len);
 char				*ft_strdup(const char *s1);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
@@ -71,6 +72,7 @@ void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void*));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
-t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+					void (*del)(void *));
 
 #endif
